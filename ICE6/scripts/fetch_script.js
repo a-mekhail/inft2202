ac12b50296ecf69ebf178a2f5cac88c448002c8b
andrew <andrewmekhail@pm.me>
2024-02-20 18:03:02 -0500

    Implemented fetch request to retrieve random joke

--- ICE6/scripts/fetch_script.js
+++ ICE6/scripts/fetch_script.js
@@ -1,16 +1,25 @@
 console.log("fetch_script.js loaded");
 
 // get the button for fetch
-
+let btn_fetch = $("#fetchJoke");
 // create a url variable
-
+let url_fetch = "https://icanhazdadjoke.com/";
 //need a header Accept value set to 'application/json'
-
+let headers_fetch = {"Accept": "application/json"};
 // create the callback for the click
+$(btn_fetch).click(() => {
     // use fetch
+    fetch(url_fetch, {
         // give the necessary header data
-        // first then() to recieve the promise
-            // send the json from the promise on to the next then()
-            // console log the json
-            // set the output
-
+        headers: headers_fetch,
+    // first then() to recieve the promise
+    }).then((res) => {
+        // send the json from the promise on to the next then()
+        return res.json();
+    }).then((jsonRes) => {
+        // console log the json
+        console.log(jsonRes);
+        // set the output
+        $("#output").text(jsonRes.joke);
+    })
+});
