718f61715e79c741bd8e16ba8b35fcf91ee6e778
andrew <andrewmekhail@pm.me>
2024-03-08 23:09:52 -0500

    ICE7 starter files

---
+++ ICE7/scripts/pixabay.js
@@ -0,0 +1,40 @@
+/**
+ * ADD HEADER COMMENTS WITH YOUR NAME + DATE
+ */
+console.log('pixabay.js loaded');
+
+// API Key for Pixabay
+const PIXABAY_API_KEY = ''; // normally we would load a key through the .env file
+// URL for Pixbay request
+const PIXABAY_URL = 'https://pixabay.com/api/?key=<API_KEY>';
+// Constant for image count
+
+
+/**
+ * makePosts
+ * Creates posts for pictures.
+ */
+const makePosts = () => 
+{
+    // make HTML elements for posts using jQuery, same number as images retrieving
+        // use bootstrap cards and append to
+        // i.e. https://getbootstrap.com/docs/5.2/components/card/#about
+        // create card
+        // card body 
+        // card title
+        // image
+        // card text
+
+};
+
+/**
+ * getPictures
+ * retrieves the pictures from Pixabay API
+ */
+const getPictures = () => 
+{
+    // use fetch to get the pictures from the API
+    // handle error(s) with .catch()
+};
+
+makePosts();
