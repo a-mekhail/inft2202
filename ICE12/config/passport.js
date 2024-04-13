b57f04a955119b71fc81030bb171a4167704b741
andrew <andrewmekhail@pm.me>
2024-04-12 23:11:27 -0400

    Ice 12 changes

--- ICE12/config/passport.js
+++ ICE12/config/passport.js
@@ -13,11 +13,51 @@ const User = connection.models.User;
 //this is what will trigger a failed login
 // trigger successful login
 // trigger failed login
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            // Find the user by username
+            const user = await User.findOne({ username });
+
+            // If the user does not exist
+            if (!user) {
+                // Trigger failed login
+                return done(null, false, { message: 'Incorrect username' });
+            }
+
+            // Check if the password is valid
+            const isValid = await validPassword(password, user.hash, user.salt);
+
+            // If the password is not valid
+            if (!isValid) {
+                // Trigger failed login
+                return done(null, false, { message: 'Incorrect password' });
+            }
+
+            // Trigger successful login
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    })
+);
 
 //LocalStrategy is a passport.js strategy that allows you to authenticate users based on a username and password. It is used to verify user credentials by checking if the username and password are valid.
 
 // In Passport.js, serializeUser and deserializeUser are two methods that are used to store and retrieve user data from a session. The serializeUser method is used to determine which data of the user object should be stored in the session.
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
 // The result of the serializeUser method is attached to the session as req.session.passport.user = { ~~ serialized user object }.
 
 // The deserializeUser method is used to retrieve the whole object from the session. It should return the full object to be attached to the request as req.user.
+module.exports = passport;
--- ICE12/lib/passwordUtils.js
+++ ICE12/lib/passwordUtils.js
@@ -1,7 +1,19 @@
 const crypto = require('crypto');
 
 // GENERATE A PASSWORD TO SAVE TO THE DATABASE -POSTREGISTER IN USERCONTROLLER.JS USES THIS
+function generatePassword(password) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    return {
+        salt: salt,
+        hash: hash
+    };
+}
 
 // RETRIEVE THE PASSWORD AND CHECK WITH THE SAME HASH APPLIED TO LOGIN PASSWORD - VERIFYCALLBACK IN PASSPORT.JS USES THIS
+function validPassword(password, salt, hash) {
+    const hashVerify = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    return hash === hashVerify;
+}
 
 module.exports = { generatePassword, validPassword };
